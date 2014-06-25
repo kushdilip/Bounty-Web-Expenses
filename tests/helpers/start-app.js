22a30ccb51aaab3654d092310ac259774b8c2388
kushdilip <kush.dilip@gmail.com>
2014-06-26 00:28:56 +0530

    Empty commit for Heroku deployment

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -18,9 +18,9 @@ export default function startApp(attrs) {
   });
 
   Ember.run(function(){
-    App = Application.create(attributes);
-    App.setupForTesting();
-    App.injectTestHelpers();
+    // App = Application.create(attributes);
+    // App.setupForTesting();
+    // App.injectTestHelpers();
   });
 
   App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
